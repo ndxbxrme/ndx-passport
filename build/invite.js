0180c443607af30193836deb2f5a1b7f8ac4a8a5
ndxbxrme <lewis_the_cat@hotmail.com>
2017-07-20 07:20:28 +0100

    invites

--- build/invite.js
+++ build/invite.js
@@ -1,6 +1,7 @@
 (function() {
   'use strict';
   module.exports = function(ndx) {
+    var tokenFromUser, userFromToken;
     if (ndx.settings.HAS_INVITE || process.env.HAS_INVITE) {
       if (typeof btoa === 'undefined') {
         global.btoa = function(str) {
@@ -12,6 +13,40 @@
           return new Buffer(b64Encoded, 'base64').toString();
         };
       }
+      userFromToken = function(token, cb) {
+        var parseToken;
+        parseToken = function(token, cb) {
+          var e, error;
+          try {
+            return cb(null, JSON.parse(ndx.parseToken(atob(token), true)));
+          } catch (error) {
+            e = error;
+            return cb(e);
+          }
+        };
+        if (ndx.shortToken) {
+          return ndx.shortToken.fetch(token, function(err, _token) {
+            if (err) {
+              return cb(err);
+            } else {
+              return parseToken(_token, cb);
+            }
+          });
+        } else {
+          return parseToken(token, cb);
+        }
+      };
+      tokenFromUser = function(user, cb) {
+        var token;
+        token = encodeURIComponent(btoa(ndx.generateToken(JSON.stringify(user), null, 4 * 24, true)));
+        if (ndx.shortToken) {
+          return ndx.shortToken.generate(token, function(shortToken) {
+            return cb(shortToken);
+          });
+        } else {
+          return cb(token);
+        }
+      };
       ndx.invite = {
         fetchTemplate: function(data, cb) {
           return cb({
@@ -23,38 +58,35 @@
         users: ['admin', 'superadmin']
       };
       ndx.app.post('/invite/accept', function(req, res, next) {
-        var e, error, user;
-        try {
-          user = JSON.parse(ndx.parseToken(atob(req.body.code), true));
-        } catch (error) {
-          e = error;
-          return next(e);
-        }
-        return ndx.database.select(ndx.settings.USER_TABLE, {
-          where: {
-            local: {
-              email: user.local.email
-            }
+        return userFromToken(req.body.code, function(err, user) {
+          if (err) {
+            return next(err);
+          } else {
+            return ndx.database.select(ndx.settings.USER_TABLE, {
+              where: {
+                local: {
+                  email: user.local.email
+                }
+              }
+            }, function(users) {
+              if (users && users.length) {
+                return next('User already exists');
+              }
+              ndx.extend(user, req.body.user);
+              user.local.password = ndx.generateHash(user.local.password);
+              ndx.database.insert(ndx.settings.USER_TABLE, user);
+              return res.end('OK');
+            });
           }
-        }, function(users) {
-          if (users && users.length) {
-            return next('User already exists');
-          }
-          ndx.extend(user, req.body.user);
-          user.local.password = ndx.generateHash(user.local.password);
-          ndx.database.insert(ndx.settings.USER_TABLE, user);
-          return res.end('OK');
         });
       });
       ndx.app.get('/invite/:code', function(req, res, next) {
-        var e, error, user;
-        try {
-          user = JSON.parse(ndx.parseToken(atob(req.params.code), true));
-        } catch (error) {
-          e = error;
-          return next(e);
-        }
-        return res.redirect("/invited?" + (encodeURIComponent(req.params.code)));
+        return userFromToken(req.params.code, function(err, user) {
+          if (err) {
+            return next(err);
+          }
+          return res.redirect("/invited?" + (encodeURIComponent(req.params.code)));
+        });
       });
       return ndx.app.post('/api/get-invite-code', ndx.authenticate(ndx.invite.users), function(req, res, next) {
         return ndx.database.select(ndx.settings.USER_TABLE, {
@@ -64,24 +96,25 @@
             }
           }
         }, function(users) {
-          var host, token;
           if (users && users.length) {
             return next('User already exists');
           }
-          token = encodeURIComponent(btoa(ndx.generateToken(JSON.stringify(req.body), req.ip, 4 * 24, true)));
-          host = process.env.HOST || ndx.settings.HOST || (req.protocol + "://" + req.hostname);
-          token = host + "/invite/" + token;
-          return ndx.invite.fetchTemplate(req.body, function(inviteTemplate) {
-            if (ndx.email) {
-              ndx.email.send({
-                to: req.body.local.email,
-                from: inviteTemplate.from,
-                subject: inviteTemplate.subject,
-                body: inviteTemplate.body,
-                code: token
-              });
-            }
-            return res.end(token);
+          return tokenFromUser(req.body, function(token) {
+            var host;
+            host = process.env.HOST || ndx.settings.HOST || (req.protocol + "://" + req.hostname);
+            token = host + "/invite/" + token;
+            return ndx.invite.fetchTemplate(req.body, function(inviteTemplate) {
+              if (ndx.email) {
+                ndx.email.send({
+                  to: req.body.local.email,
+                  from: inviteTemplate.from,
+                  subject: inviteTemplate.subject,
+                  body: inviteTemplate.body,
+                  code: token
+                });
+              }
+              return res.end(token);
+            });
           });
         });
       });
--- src/invite.coffee
+++ src/invite.coffee
@@ -8,6 +8,27 @@ module.exports = (ndx) ->
     if typeof atob is 'undefined'
       global.atob = (b64Encoded) ->
         new Buffer(b64Encoded, 'base64').toString()
+    userFromToken = (token, cb) ->
+      parseToken = (token, cb) ->
+        try
+          cb null, JSON.parse ndx.parseToken(atob(token), true)
+        catch e
+          return cb e
+      if ndx.shortToken
+        ndx.shortToken.fetch token, (err, _token) ->
+          if err
+            cb err
+          else
+            parseToken _token, cb
+      else
+        parseToken token, cb
+    tokenFromUser = (user, cb) ->
+      token = encodeURIComponent(btoa(ndx.generateToken(JSON.stringify(user), null, 4 * 24, true)))
+      if ndx.shortToken
+        ndx.shortToken.generate token, (shortToken) ->
+          cb shortToken
+      else
+        cb token
     ndx.invite = 
       fetchTemplate: (data, cb) ->
         cb
@@ -16,27 +37,26 @@ module.exports = (ndx) ->
           from: "System"
       users: ['admin', 'superadmin']
     ndx.app.post '/invite/accept', (req, res, next) ->
-      try
-        user = JSON.parse ndx.parseToken(atob(req.body.code), true)
-      catch e
-        return next e
-      ndx.database.select ndx.settings.USER_TABLE,
-        where:
-          local:
-            email: user.local.email
-      , (users) ->
-        if users and users.length
-          return next 'User already exists'
-        ndx.extend user, req.body.user
-        user.local.password = ndx.generateHash user.local.password
-        ndx.database.insert ndx.settings.USER_TABLE, user
-        res.end 'OK'
+      userFromToken req.body.code, (err, user) ->
+        if err
+          return next err
+        else
+          ndx.database.select ndx.settings.USER_TABLE,
+            where:
+              local:
+                email: user.local.email
+          , (users) ->
+            if users and users.length
+              return next 'User already exists'
+            ndx.extend user, req.body.user
+            user.local.password = ndx.generateHash user.local.password
+            ndx.database.insert ndx.settings.USER_TABLE, user
+            res.end 'OK'
     ndx.app.get '/invite/:code', (req, res, next) ->
-      try
-        user = JSON.parse ndx.parseToken(atob(req.params.code), true)
-      catch e
-        return next e
-      res.redirect "/invited?#{encodeURIComponent(req.params.code)}"
+      userFromToken req.params.code, (err, user) ->
+        if err
+          return next err
+        res.redirect "/invited?#{encodeURIComponent(req.params.code)}"
     ndx.app.post '/api/get-invite-code', ndx.authenticate(ndx.invite.users), (req, res, next) ->
       ndx.database.select ndx.settings.USER_TABLE,
         where:
@@ -45,15 +65,15 @@ module.exports = (ndx) ->
       , (users) ->
         if users and users.length
           return next 'User already exists'
-        token = encodeURIComponent(btoa(ndx.generateToken(JSON.stringify(req.body), req.ip, 4 * 24, true)))
-        host = process.env.HOST or ndx.settings.HOST or "#{req.protocol}://#{req.hostname}"
-        token = "#{host}/invite/#{token}"
-        ndx.invite.fetchTemplate req.body, (inviteTemplate) ->
-          if ndx.email
-            ndx.email.send
-              to: req.body.local.email
-              from: inviteTemplate.from
-              subject: inviteTemplate.subject
-              body: inviteTemplate.body
-              code: token
-          res.end token
+        tokenFromUser req.body, (token) ->
+          host = process.env.HOST or ndx.settings.HOST or "#{req.protocol}://#{req.hostname}"
+          token = "#{host}/invite/#{token}"
+          ndx.invite.fetchTemplate req.body, (inviteTemplate) ->
+            if ndx.email
+              ndx.email.send
+                to: req.body.local.email
+                from: inviteTemplate.from
+                subject: inviteTemplate.subject
+                body: inviteTemplate.body
+                code: token
+            res.end token
